d6b54726ccef6dddb3df27aa0bb30f84f00bc003
unknown <tim@(none)>
2011-10-13 14:29:17 +0100

    updates left, right and centre

--- lat-long-to-postcode.php
+++ lat-long-to-postcode.php
@@ -4,6 +4,9 @@ require_once('library/http.php');
 require_once('library/debug.php');
 require_once('library/facebook/facebook.php');
 
+$lat = HttpData::getQueryAsString('lat');
+$lon = HTtpData::getQueryAsString('lon');
+
 $response = new stdClass;
 $response->postcode = "HP6 5JW";
 
---
+++ vars.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('library/utils.php');
+ErrorHandler::Enable();
+define('NEARBY', '3055bbe8ba5320');
+define('GOOGLE_API', 'ABQIAAAAVpSB4ZP0FjpvpW0IkxhAzBQDimnP5PSCPZPDxCK880mVupYQ6hQsleyQELV68SdW2GbrMMUvgowJIg');
+define('FACEBOOK_SECRET_ID', '37ec34f0592e31618e629a97edc9171c');
+define('FACEBOOK_APP_ID', '169367879817491');
